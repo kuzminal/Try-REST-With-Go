2f1e717787fbb716ebd9234b09003e5d16bbd829
kuzminal <kuzmin.al35@gmail.com>
2021-09-16 17:08:17 +0300

    Путей не прибавилось, но один и обработчиков теперь принимает регулярку

--- rest_handler.go
+++ rest_handler.go
@@ -3,35 +3,41 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"path"
+	"regexp"
 	"strings"
 )
 
 func main() {
-	pr := newPathResolver()
-	pr.Add("GET /hello", hello)
-	pr.Add("* /goodbye/*", goodbye)
-	http.ListenAndServe(":8080", pr)
-}
-func newPathResolver() *pathResolver {
-	return &pathResolver{make(map[string]http.HandlerFunc)}
+	rr := newPathResolver()
+	rr.Add("GET /hello", hello)
+	rr.Add("(GET|HEAD) /goodbye(/?[A-Za-z0-9]*)?", goodbye)
+	http.ListenAndServe(":8080", rr)
 }
 
-type pathResolver struct {
+func newPathResolver() *regexResolver {
+	return &regexResolver{
+		handlers: make(map[string]http.HandlerFunc),
+		cache:    make(map[string]*regexp.Regexp),
+	}
+}
+
+type regexResolver struct {
 	handlers map[string]http.HandlerFunc
+	cache    map[string]*regexp.Regexp
 }
 
-func (p *pathResolver) Add(path string, handler http.HandlerFunc) {
-	p.handlers[path] = handler
+func (r *regexResolver) Add(regex string, handler http.HandlerFunc) {
+	r.handlers[regex] = handler
+	cache, _ := regexp.Compile(regex)
+	r.cache[regex] = cache
 }
-func (p *pathResolver) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+
+func (r *regexResolver) ServeHTTP(res http.ResponseWriter, req *http.Request) {
 	check := req.Method + " " + req.URL.Path
-	for pattern, handlerFunc := range p.handlers {
-		if ok, err := path.Match(pattern, check); ok && err == nil {
+	for pattern, handlerFunc := range r.handlers {
+		if r.cache[pattern].MatchString(check) == true {
 			handlerFunc(res, req)
 			return
-		} else if err != nil {
-			fmt.Fprint(res, err)
 		}
 	}
 	http.NotFound(res, req)
@@ -47,9 +53,9 @@ func hello(res http.ResponseWriter, req *http.Request) {
 func goodbye(res http.ResponseWriter, req *http.Request) {
 	path := req.URL.Path
 	parts := strings.Split(path, "/")
-	name := parts[2]
-	if name == "" {
-		name = "Aleksey Kuzmin"
+	name := "Aleksey Kuzmin"
+	if len(parts) > 2 {
+		name = parts[2]
 	}
 	fmt.Fprint(res, "Goodbye ", name)
 }
